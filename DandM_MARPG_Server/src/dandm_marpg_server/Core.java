c508d49e48ca5e2d014035f6fde0c870b7c2fc46
D Win <dwin@wintech.net.nz>
2013-03-08 17:39:55 +1300

    MySQL Class
    Core Class and MySQL Class added

---
+++ DandM_MARPG_Server/src/dandm_marpg_server/Core.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dandm_marpg_server;
+
+/**
+ *
+ * @author Dajne Win
+ */
+public class Core {
+    
+    
+    
+    public void Core()
+    {
+        
+    }
+    
+}
---
+++ DandM_MARPG_Server/src/dandm_marpg_server/MySQLDatabase.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dandm_marpg_server;
+
+/**
+ *
+ * @author Dajne Win
+ */
+public class MySQLDatabase {
+    
+    private String url = "jdbc:mysql://wintech.net.nz:3306/MARPG";
+    private String user = "";
+    private String password = "";
+    
+}
