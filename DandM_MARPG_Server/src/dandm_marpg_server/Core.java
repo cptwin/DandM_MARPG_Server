babe56db67cc3856ea40946450d9798d9a143588
D Win <dwin@wintech.net.nz>
2013-03-11 19:01:01 +1300

    Login + Registration
    Basic Login and Registration

--- DandM_MARPG_Server/src/dandm_marpg_server/Core.java
+++ DandM_MARPG_Server/src/dandm_marpg_server/Core.java
@@ -8,8 +8,11 @@ import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -18,6 +21,9 @@ import java.util.logging.Logger;
  * @author Dajne Win
  */
 public class Core {
+    
+    private String username = "";
+    private String password = "";
     
     
     
@@ -32,12 +38,13 @@ public class Core {
     
     private void startListening() throws IOException
     {
+        
         String clientSentence;
-         String capitalizedSentence;
-         ServerSocket welcomeSocket = new ServerSocket(6789);
+        String capitalizedSentence;
+        ServerSocket welcomeSocket = new ServerSocket(6789);
 
-         while(true)
-         {
+        while(true)
+        {
             Socket connectionSocket = welcomeSocket.accept();
             BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
             DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
@@ -48,11 +55,46 @@ public class Core {
                 String[] str_array = clientSentence.split(" ");
                 if (str_array.length == 3)
                 {
-                    outToClient.writeBytes("Attempting to login!" + '\n');
+                    if (username.equals(str_array[1]))
+                    {
+                        if (password.contentEquals(sha512Hash(str_array[2])))
+                        {
+                            outToClient.writeBytes("You are now logged in!" + '\n');
+                        }
+                        else
+                        {
+                            outToClient.writeBytes("Incorrect Password!" + '\n');
+                        }
+                    }
+                    else
+                    {
+                        outToClient.writeBytes("Username doesn't exist!" + '\n');
+                    }
                 }
                 else
                 {
                     outToClient.writeBytes("Invalid login!" + '\n');
+                }
+            }
+            else if (clientSentence.startsWith("register"))
+            {
+                String[] str_array = clientSentence.split(" ");
+                if (str_array.length == 3)
+                {
+                    if (str_array[1].length() > 3 && str_array[2].length() > 3)
+                    {
+                        username = str_array[1];
+                        password = sha512Hash(str_array[2]);
+                        outToClient.writeBytes("Successfully registered " + username + '\n');
+                    }
+                    else
+                    {
+                        outToClient.writeBytes("Problem with Registration!" + '\n');
+                    }
+                }
+                else
+                {
+                    outToClient.writeBytes("Invalid Registration!" + '\n');
                 }
             }
             else
@@ -60,6 +102,30 @@ public class Core {
                 outToClient.writeBytes("Invalid Command!" + '\n');
             }
          }
+    }
+    
+    private String sha512Hash(String stringToHash)
+    {
+        String output = "";
+        StringBuilder sha512Hash = new StringBuilder();
+        MessageDigest md = null;
+        try {
+
+            md = MessageDigest.getInstance("SHA-256");
+            md.update(stringToHash.getBytes("UTF-8"));
+        } catch (UnsupportedEncodingException | NoSuchAlgorithmException ex) {
+            Logger.getLogger(Core.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        byte[] sha512ByteHash = md.digest();
+        for (int i = 0; i < sha512ByteHash.length; i++) {
+            if ((0xff & sha512ByteHash[i]) < 0x10) {
+                sha512Hash.append("0").append(Integer.toHexString((0xFF & sha512ByteHash[i])));
+            } else {
+                sha512Hash.append(Integer.toHexString(0xFF & sha512ByteHash[i]));
+            }
+        }
+        output = sha512Hash.toString();
+        return output;
     }
     
 }
